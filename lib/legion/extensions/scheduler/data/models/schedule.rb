0eee43602dd748e25df58a780fe17208abe5d70b
Esity <matthewdiverson@gmail.com>
2020-10-22 20:50:13 -0500

    commenting out relationship

--- lib/legion/extensions/scheduler/data/models/schedule.rb
+++ lib/legion/extensions/scheduler/data/models/schedule.rb
@@ -1,6 +1,7 @@
 module Legion::Extensions::Scheduler::Data::Model
   class Schedule < Sequel::Model
     one_to_many :schedule_logs
+    # many_to_one :task, class: Legion::Data::Model::Task
     many_to_one :function, class: "::Legion::Data::Model::Function"
   end
 end
